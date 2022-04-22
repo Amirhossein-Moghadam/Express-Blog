55aed296664d20a7c0f937987d8ff7f5c836cc08
Amirhossein Moghadam <Amirhoussein.Moghadam79@gmail.com>
2022-04-22 16:51:15 +0430

    feat: server

---
+++ app.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+const express = require("express");
+const dotEnv = require("dotEnv");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+const indexRoutes = require("./routes");
+
+//* Load Config
+dotEnv.config({ path: "./config/config.env" });
+
+//* Database Connection
+connectDB();
+
+const app = express();
+
+//* Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//* View Engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//* Static Folders
+app.use(express.static(path.join(__dirname, "public")));
+
+//* Routes
+app.use(indexRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
