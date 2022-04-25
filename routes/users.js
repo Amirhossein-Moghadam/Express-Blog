4373585f31c4b71d95404952e9c71cfb5272a643
Amirhossein Moghadam <Amirhoussein.Moghadam79@gmail.com>
2022-04-26 03:01:48 +0430

    feat: user routes

--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,19 @@
 const { Router } = require("express");
-const Yup = require("Yup");
+
+const userControllers = require("../controllers/userControllers");
 
 const router = new Router();
 
-const schema = Yup.object().shape({
-  fullname: Yup.string().min(4).max(255).required(),
-  email: Yup.string().email().required(),
-  password: Yup.string().min(4).max(255).required(),
-  confirmPassword: Yup.string()
-    .required()
-    .oneOf([Yup.ref("password"), null]),
-});
-
 // @desc: login page
 // route: GET /users/login
-router.get("/login", (req, res) => {
-  res.render("login", { pageTitle: "ورود به بخش مدیریت", path: "/login" });
-});
+router.get("/login", userControllers.login);
 
 // @desc: register page
 // route: GET /users/register
-router.get("/register", (req, res) => {
-  res.render("register", {
-    pageTitle: "ثبت نام کاربر جدید",
-    path: "/register",
-  });
-});
+router.get("/register", userControllers.register);
 
 // @desc: register hndle
 // route: POST /users/register
-router.post("/register", (req, res) => {
-  //   const validator = schema.isValid(req.body);
-  //   validator
-  //     .then((result) => {
-  //       if (result === true) {
-  //         res.send("All Good");
-  //       } else {
-  //         res.send("Not Valid");
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       res.send("Error");
-  //       console.log(error);
-  //     });
+router.post("/register", userControllers.createUser);
 
-  schema
-    .validate(req.body)
-    .then((result) => {
-      console.log(result);
-      res.send("Is valid");
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send("Not Valid");
-    });
-});
 module.exports = router;
