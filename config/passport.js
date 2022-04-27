4fab38bbef854b8ba218e4ecdff6d69eb02544bf
Amirhossein Moghadam <Amirhoussein.Moghadam79@gmail.com>
2022-04-27 10:18:59 +0430

    feat: passport config

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+const { Strategy } = require("passport-local");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/User");
+
+passport.use(
+  new Strategy({ usernameField: "email" }, async (email, password, done) => {
+    try {
+      const user = User.findOne({ email });
+      if (!user) {
+        return done(null, false, {
+          message: "کاربری با این ایمیل ثبت نشده است",
+        });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) return done(null, user); //req.user
+      return done(null, false, {
+        message: "نام کاربری یا رمز عبور اشتباه است",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  })
+);
